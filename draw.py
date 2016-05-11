af702d57f71e6bd8587b38b3c6dd7408e7ace7f5
bakslash <marcusmuasa@gmail.com>
2016-05-11 22:54:32 +0300

    draw program

---
+++ draw.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("orange")
+    
+    brad = turtle.Turtle()
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+    brad.forward(100)
+    brad.right(90)
+
+    window.exitonclick()
+
+draw_square()
